1f9966316e72d71d73f0d0f74d8c2e40288db29e
Farahani <nurulfarahanisahiri123@gmail.com>
2021-10-14 16:00:39 +0800

    first commit

---
+++ src/components/Portfolio.vue
@@ -0,0 +1,56 @@
+<template>
+<!-- Portfolio Grid-->
+        <section class="page-section bg-light" id="portfolio">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Portfolio</h2>
+                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-sm-6 mb-4" v-for="(obj,key) in portfolioJSON" :key="key">
+                        <!-- Portfolio item 1-->
+                        <div class="portfolio-item" >
+                            <a class="portfolio-link" data-bs-toggle="modal" href="#portfolioModal1">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                                </div>
+                                <img class="img-fluid" src="/assets/img/portfolio/close-icon.svg" alt="..." />
+                            </a>
+                            <div class="portfolio-caption">
+                                <div class="portfolio-caption-heading">{{ obj.title }}</div>
+                                <div class="portfolio-caption-subheading text-muted">{{ obj.caption }}</div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </section>
+</template>
+
+<script>
+export default{
+data : () => ({
+    portfolioJSON: [
+        {
+            img: '../src/assets/img/navbar-logo.svg',
+            caption: 'first logo',
+            title: 'Graphic design'
+        },
+        {
+            img: '/src/assets/img/navbar-logo.svg',
+            caption: 'second logo',
+            title: 'Programming'
+        },
+        {
+            img: '/src/assets/img/navbar-logo.svg',
+            caption: 'third logo',
+            title: 'Development'
+        }
+    ]
+})
+}
+</script>
+
+<style lang="saas">
+</style>
---
+++ src/components/Services.vue
@@ -0,0 +1,47 @@
+<template>
+        <!-- Services-->
+        <section class="page-section" id="services">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Services</h2>
+                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                </div>
+                <div class="row text-center" >
+                    <div class="col-md-4" v-for="(obj,key) in servicesJSON" :key="key">
+                        <span class="fa-stack fa-4x">
+                        <i class="fas fa-circle fa-stack-2x text-primary"></i><i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <h4 class="my-3">{{ obj.title }}</h4>
+                        <p class="text-muted">{{ obj.caption }}</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+</template>
+
+<script>
+export default{
+data : () => ({
+    servicesJSON: [
+        {
+            img: '<i class="fas fa-circle fa-stack-2x text-primary"></i><i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>',
+            caption: 'e-commerce embedded with payment system',
+            title: 'E-Commerce'
+        },
+        {
+            img: '<i class="fas fa-circle fa-stack-2x text-primary"></i><i class="fas fa-laptop fa-stack-1x fa-inverse"></i>',
+            caption: 'best responsive design',
+            title: 'Responsive Design'
+        },
+        {
+            img: '<i class="fas fa-circle fa-stack-2x text-primary"></i><i class="fas fa-lock fa-stack-1x fa-inverse"></i>',
+            caption: 'included responsive design',
+            title: 'Web Security'
+        }
+    ]
+})
+}
+</script>
+
+<style lang="saas">
+</style>
